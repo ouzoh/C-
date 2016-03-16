6f1db4f14c63c58b3ea9464fa9d3dd7c22699ac8
System Administrator <root@iMac.local>
2016-03-16 00:05:11 -0400

    first commit

---
+++ greetings.cpp
@@ -0,0 +1,13 @@
+/*
+greetings.cpp
+15 Mar 2016
+Ogaga Uzoh
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout <<"Hello, and Welcome to Up and Running with C++ March 2016"<<endl;
+
+return 0;
+}
